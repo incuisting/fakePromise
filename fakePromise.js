9119c5668217e0ca979457ce579f87229aec89aa
incuisting <xuyanting9634@163.com>
2018-05-17 22:37:55 +0800

    对循环引用进行检测

--- fakePromise.js
+++ fakePromise.js
@@ -29,26 +29,35 @@ function FakePromise(executor) {
     reject(error)
   }
 }
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    reject(new TypeError('自己引用自己'))
+  }
+}
 FakePromise.prototype.then = function(onFulfilled, onRejected) {
   let self = this
   let promise2
   if (self.status === 'resolved') {
     promise2 = new FakePromise(function(resolve, reject) {
       let x = onFulfilled(self.value)
+      resolvePromise(promise2, x, resolve, reject)
     })
   }
   if (self.status === 'rejected') {
     promise2 = new FakePromise(function(resolve, reject) {
       let x = onRejected(self.reason)
+      resolvePromise(promise2, x, resolve, reject)
     })
   }
   if (self.status === 'pendding') {
     promise2 = new FakePromise(function(resolve, reject) {
       self.onResolvedCallbacks.push(function() {
         let x = onFulfilled(self.value)
+        resolvePromise(promise2, x, resolve, reject)
       })
       self.onRejectedCallbacks.push(function() {
         let x = onRejected(self.reason)
+        resolvePromise(promise2, x, resolve, reject)
       })
     })
   }
