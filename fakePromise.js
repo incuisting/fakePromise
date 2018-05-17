cf55dcc1e4a47907ead37e5dfba6b401d67f86c3
incuisting <xuyanting9634@163.com>
2018-05-17 21:13:46 +0800

    默认的成功值，默认的失败原因，初始状态

--- fakePromise.js
+++ fakePromise.js
@@ -1,4 +1,8 @@
 function FakePromise(executor) {
+  let self = this
+  self.status = 'pendding'
+  self.value = undefined
+  self.reason = undefined
   function resolve() {}
   function reject() {}
   executor(resolve, reject)
