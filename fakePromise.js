55b89b1f4fab84b097c5cdaeee2578835f298589
incuisting <xuyanting9634@163.com>
2018-05-17 21:19:12 +0800

    resolve 和 reject 判断pendding，然后改变状态和值

--- fakePromise.js
+++ fakePromise.js
@@ -3,8 +3,18 @@ function FakePromise(executor) {
   self.status = 'pendding'
   self.value = undefined
   self.reason = undefined
-  function resolve() {}
-  function reject() {}
+  function resolve(value) {
+    if (self.status === 'pendding') {
+      self.status = 'resolve'
+      self.value = value
+    }
+  }
+  function reject(reason) {
+    if (self.status === 'pendding') {
+      self.status = 'reject'
+      self.reason = reason
+    }
+  }
   executor(resolve, reject)
 }
 FakePromise.prototype.then = function(onFulfilled, onRejected) {}
